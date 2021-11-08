4617b348d555815de31ac77c331c416fcb246d30
KateMila <89808903+KateMila@users.noreply.github.com>
2021-11-08 11:53:25 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 6000;
+        int miles = 20;
+
+        int bonus = price / miles;
+        System.out.println(bonus);
+    }
+}
+
